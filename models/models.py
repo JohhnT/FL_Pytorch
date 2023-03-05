56e1ba2c6517df1703dfc14a49aa74c325ee2c1e
mtuantr <minhtuan.tran@uq.net.au>
2023-03-05 16:06:54 +1000

    fix FashionMNIST model

--- models/models.py
+++ models/models.py
@@ -5,6 +5,28 @@ from torch import nn
 def get_model(dataset_name):
     if dataset_name == "MNIST":
         return Mnist_Net()
+    elif dataset_name == "FashionMNIST":
+        return FashionMnist_Net()
+
+
+class FashionMnist_Net(nn.Module):
+    def __init__(self):
+        super(FashionMnist_Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(32, 64, 5)
+        self.fc = nn.Sequential(
+            nn.Linear(64 * 4 * 4, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10)
+        )
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 4 * 4 * 64)
+        x = self.fc(x)
+        return x
 
 
 class Mnist_Net(nn.Module):
