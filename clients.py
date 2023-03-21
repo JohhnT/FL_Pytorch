023a8e4c131c83011b9f8e48711be2b674e062c6
mtuantr <minhtuan.tran@uq.net.au>
2023-03-21 16:58:06 +1000

    add calculation of benign iteration weight mean

--- clients.py
+++ clients.py
@@ -14,7 +14,11 @@ class Client:
         self.num = self.config.clients.total
         self.compromised = self.config.clients.compromised
         self.compromised_id = [i for i in range(0, self.compromised)]
+        # we initialize benign_iteration at 1st round only
+        # other consequent rounds use this iteration to
+        # compute malicious updates
         self.benign_iteration = [None for _ in range(0, self.compromised)]
+        self.benign_mean = None
         self.compromised_attack = self.config.clients.compromised_attack
         self.client_id = [i for i in range(self.compromised, self.num)]
         self.model = None
@@ -47,15 +51,32 @@ class Client:
         """
         Compromised client's local train function
         """
-        if self.benign_iteration[user_id]:
 
-            #
+        # key steps
+        # 1. compute mean of compromised clients' benign updates (done)
+        # 2. compute direction deviating from self.model
+        # 3. find optimised lambda
+        # 4. find w1 (malicious)
+        # 5. randomly select w2..wc
+        # 6. append updates
+        #
+        # We do step 1 during first iteration, since the benign iteration is only computed once
+        # Step 2-5 is only done by the first selected compromised client in the current iteration (due to data structure)
+        # List of crafted w1..wc is stored during the iteration
+        # At step 6, compromised client select crafted updates based on their user_id
+
+        if self.benign_iteration[user_id]:
             benign_iter = self.benign_iteration[user_id]
             model, epoch_loss, running_corrects, len_dataset = benign_iter["model"], benign_iter[
                 "epoch_loss"], benign_iter["running_corrects"], benign_iter["len_dataset"]
+
+            if self.benign_mean is None:
+                self.compute_benign_mean()
+
+            #
             lock = threading.Lock()
             lock.acquire()
-            self.weights.append(model)
+            self.weights.append(self.benign_mean)
             self.epoch_loss.append(epoch_loss)
             self.running_corrects.append(running_corrects)
             self.len_dataset.append(len_dataset)
@@ -69,6 +90,20 @@ class Client:
                 "running_corrects": running_corrects,
                 "len_dataset": len_dataset
             }
+
+    def compute_benign_mean(self):
+        weights = []
+        length = []
+        for d in self.benign_iteration:
+            weights.append(d["model"])
+            length.append(d["len_dataset"])
+        w_avg = copy.deepcopy(weights[0])
+        for k in w_avg.keys():
+            w_avg[k] *= length[0]
+            for i in range(1, len(weights)):
+                w_avg[k] += weights[i][k] * length[i]
+            w_avg[k] = w_avg[k] / (sum(length))
+        self.benign_mean = w_avg
 
     def benign_local_train(self, user_id, dataloaders, verbose=1):
         """
