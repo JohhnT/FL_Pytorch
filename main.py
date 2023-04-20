9d208ad460f26808473365ce17447d1b08c620c2
mtuantr <minhtuan.tran@uq.net.au>
2023-04-21 01:29:57 +1000

    add logging

--- main.py
+++ main.py
@@ -14,12 +14,23 @@ if __name__ == "__main__":
                         help='Log messages level.')
     parser.add_argument('-d', '--dataset', type=str, default='BreastCancer',
                         help='the name of dataset')
+    parser.add_argument('-o', '--out', type=str,
+                        default='', help='output file')
 
     args = parser.parse_args()
     # Set logging
-    logging.basicConfig(
-        format='[%(levelname)s][%(asctime)s]: %(message)s', level=getattr(logging, args.log.upper()),
-        datefmt='%H:%M:%S')
+    if args.out == '':
+        logging.basicConfig(
+            format='[%(levelname)s][%(asctime)s]: %(message)s',
+            level=getattr(logging, args.log.upper()),
+            datefmt='%H:%M:%S')
+    else:
+        logging.basicConfig(
+            filename=args.out,
+            filemode='a',
+            format='[%(levelname)s][%(asctime)s]: %(message)s',
+            level=getattr(logging, args.log.upper()),
+            datefmt='%H:%M:%S')
     logging.info("config:{},  log:{}".format(args.config, args.log))
     # load config
     config = config.Config(args.config)
