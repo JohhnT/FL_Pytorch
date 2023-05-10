8009512b688d054c5b03b2397fd825e31e1dd55b
mtuantr <minhtuan.tran@uq.net.au>
2023-05-11 00:24:27 +1000

    add gen test

---
+++ gen_tests.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+python3 main.py -c configs/MNIST/mnist-median-mean-ext.json -o out/mnist-median-mean-compromised-10-ext
+python3 main.py -c configs/BreastCancer/breastcancer-median-mean-ext.json -o out/breastcancer-median-mean-compromised-10-ext
+python3 main.py -c configs/BreastCancer/breastcancer-median-mean.json -o out/breastcancer-median-mean-compromised-10
+python3 main.py -c configs/MNIST/mnist-median-krum.json -o out/mnist-median-krum-compromised-10
+python3 main.py -c configs/MNIST/mnist-median-krum-ext.json -o out/mnist-median-krum-compromised-10-ext
+python3 main.py -c configs/BreastCancer/breastcancer-median-krum.json -o out/breastcancer-median-krum-compromised-10
+python3 main.py -c configs/BreastCancer/breastcancer-median-krum-ext.json -o out/breastcancer-median-krum-compromised-10-ext
