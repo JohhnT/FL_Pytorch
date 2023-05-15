f0d11c16ddc0a31a4ff4afe9441f60fda1e45428
mtuantr <minhtuan.tran@uq.net.au>
2023-05-16 02:15:18 +1000

    fixup

--- collect.sh
+++ collect.sh
@@ -3,9 +3,9 @@
 experiment=$1
 
 mkdir "extracted/$experiment"
-cat "out/$experiment" | grep "training acc" | head -n100 | awk '{print $4}' | awk -F ',' '{print $1}' > "extracted/$experiment/training_acc"
-cat "out/$experiment" | grep "training acc" | head -n100 | awk '{print $6}' | awk -F ',' '{print $1}' > "extracted/$experiment/test_acc"
-cat "out/$experiment" | grep "training acc" | head -n100 | awk '{print $8}' > "extracted/$experiment/test_loss"
+cat "out/$experiment" | grep "training acc" | head -n100 | awk '{print $4}' | awk -F ',' '{print $1}' > "extracted/$experiment/training acc"
+cat "out/$experiment" | grep "training acc" | head -n100 | awk '{print $6}' | awk -F ',' '{print $1}' > "extracted/$experiment/test acc"
+cat "out/$experiment" | grep "training acc" | head -n100 | awk '{print $8}' > "extracted/$experiment/test loss"
 #head -n5 "extracted/$experiment/*"
 #./plot.sh $experiment training_acc
 #./plot.sh $experiment test_acc
