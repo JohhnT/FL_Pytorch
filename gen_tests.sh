28eefb49069d181d64b009d10eba3ea0a38cbb52
mtuantr <minhtuan.tran@uq.net.au>
2023-05-17 01:26:52 +1000

    test krum

--- gen_tests.sh
+++ gen_tests.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-python3 main.py -c configs/MNIST/mnist-krum-mean.json -o out/mnist-krum-mean-compromised-50
-python3 main.py -c configs/MNIST/mnist-krum-mean-ext.json -o out/mnist-krum-mean-compromised-50-ext
-python3 main.py -c configs/BreastCancer/breastcancer-krum-mean-ext.json -o out/breastcancer-krum-mean-compromised-50-ext
-python3 main.py -c configs/BreastCancer/breastcancer-krum-mean.json -o out/breastcancer-krum-mean-compromised-50
-python3 main.py -c configs/MNIST/mnist-krum-krum.json -o out/mnist-krum-krum-compromised-50
-python3 main.py -c configs/MNIST/mnist-krum-krum-ext.json -o out/mnist-krum-krum-compromised-50-ext
-python3 main.py -c configs/BreastCancer/breastcancer-krum-krum.json -o out/breastcancer-krum-krum-compromised-50
-python3 main.py -c configs/BreastCancer/breastcancer-krum-krum-ext.json -o out/breastcancer-krum-krum-compromised-50-ext
+python3 main.py -c configs/MNIST/mnist-krum-mean.json -o out/mnist-krum-mean
+python3 main.py -c configs/MNIST/mnist-krum-mean-ext.json -o out/mnist-krum-mean-ext
+python3 main.py -c configs/BreastCancer/breastcancer-krum-mean-ext.json -o out/breastcancer-krum-mean-ext
+python3 main.py -c configs/BreastCancer/breastcancer-krum-mean.json -o out/breastcancer-krum-mean
+python3 main.py -c configs/MNIST/mnist-krum-krum.json -o out/mnist-krum-krum
+python3 main.py -c configs/MNIST/mnist-krum-krum-ext.json -o out/mnist-krum-krum-ext
+python3 main.py -c configs/BreastCancer/breastcancer-krum-krum.json -o out/breastcancer-krum-krum
+python3 main.py -c configs/BreastCancer/breastcancer-krum-krum-ext.json -o out/breastcancer-krum-krum-ext
